906b32fd429f03b7ab34629cf852149f03099aa8
Renato Farias <renatofarias@MacBook-Pro-de-Renato.local>
2020-03-22 10:33:55 -0300

    Primeiro commit

---
+++ 01.py
@@ -0,0 +1,33 @@
+import threading
+from time import sleep
+
+global bown
+# funcao que espera 1 segundo
+
+def wait():
+    cont = 0
+    global bown
+    while True:
+        bown = 0
+        print(cont)
+        cont += 1
+        sleep(1)
+        bown = 1
+        # print(bown)
+
+
+def LerVelocidade():
+    global bown
+    while True:
+        if (bown == 1):
+            print('Leitura da Velocidade')
+
+
+# ----------------criando a thread
+#adicionar mutex
+
+t = threading.Thread(target=wait, name='Wait')
+t1 = threading.Thread(target=LerVelocidade, name='Velocidade')
+t.start()
+t1.start()
+
